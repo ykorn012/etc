9e2a3cf48b54f3c1f14b896cfd93119ed2d12e67
크게접은하늘 <ykorn012@gmail.com>
2018-05-10 02:35:31 +0900

    Update VM_total12.py

--- VM_total12.py
+++ VM_total12.py
@@ -11,109 +11,15 @@ from sklearn.model_selection import KFold
 from sklearn.cross_decomposition import PLSRegression
 from sklearn.preprocessing import scale
 
-#os.chdir("D:/01. CLASS/Machine Learning/01. FabWideSimulation2/")
-
-Y_p1 = []
-A_p1 = np.array([[0.5, -0.2], [0.25, 0.15]])
-d_p1 = np.array([[0.1, 0], [0.05, 0]])
-C_p1 = np.transpose(np.array([[0, 0.5, 0.05, 0, 0.15, 0], [0.085, 0, 0.025, 0.2, 0, 0]]))
 lamda_PLS = 0.1
-
 N = 120
+DoE_Queue = []
 
-Queue = []
+def sampling(k):
+    A_p1 = np.array([[0.5, -0.2], [0.25, 0.15]])
+    d_p1 = np.array([[0.1, 0], [0.05, 0]])
+    C_p1 = np.transpose(np.array([[0, 0.5, 0.05, 0, 0.15, 0], [0.085, 0, 0.025, 0.2, 0, 0]]))
 
-for k in range(0, N): # range(101) = [0, 1, 2, ..., 100])
-    u1_p1 = np.random.normal(0.4, np.sqrt(0.2))
-    u2_p1 = np.random.normal(0.6, np.sqrt(0.2))
-    u_p1 = np.array([u1_p1, u2_p1])
-
-    v1_p1 = np.random.normal(1, np.sqrt(0.2))
-    v2_p1 = 2 * v1_p1
-    v3_p1 = np.random.uniform(0.2, 1.2)
-    v4_p1 = 3 * v3_p1
-    v5_p1 = np.random.uniform(0, 0.4)
-    v6_p1 = np.random.normal(-0.6, np.sqrt(0.2))
-
-    v_p1 = np.array([v1_p1, v2_p1, v3_p1, v4_p1, v5_p1, v6_p1])
-
-    k1_p1 = k # n = 100 일 때 #1 entity maintenance event
-    k2_p1 = k # n = 200 일 때 #1 entity maintenance event
-
-    k_p1 = np.array([[k1_p1], [k2_p1]])
-
-    psi = np.array([u1_p1, u2_p1, v1_p1, v2_p1, v3_p1, v4_p1, v5_p1, v6_p1, k1_p1, k2_p1])
-
-    e1_p1 = np.random.normal(0, np.sqrt(0.1))
-    e2_p1 = np.random.normal(0, np.sqrt(0.2))
-    e_p1 = np.array([e1_p1, e2_p1])
-
-    y_p1 = u_p1.dot(A_p1) + v_p1.dot(C_p1) + np.sum(k_p1 * d_p1, axis = 0) + e_p1
-    rows = np.r_[psi, y_p1]
-    Queue.append(rows)
-
-Queue = np.array(Queue)
-Mean_M = np.mean(Queue, axis = 0)
-Sampling_M = Queue - Mean_M
-
-Sampling_M = np.c_[Sampling_M, np.arange(N)]
-
-#np.savetxt("output/vm_total3.csv", Sampling_M, delimiter=",", fmt="%s")
-
-V = Sampling_M[:,0:10]
-y = Sampling_M[:,10:13]
-
-T = 7
-
-X_train, X_test, y_train, y_test = train_test_split(V, y, test_size=0)
-# print(X_train.shape, y_train.shape)
-# print(X_test.shape, y_test.shape)
-
-# temp = np.c_[X_train, y_train]
-#
-# np.savetxt("output/vm_total4.csv", temp, delimiter=",", fmt="%s")
-
-kf = KFold(n_splits = 10, shuffle=True, random_state = 1)
-
-# for i in np.arange(1, T):
-#     print('====================================================')
-#     pls = PLSRegression(n_components = i, scale=False)
-#     pls.fit(X_train, y_train[:,0:2])
-#     scores = pls.score(X_train, y_train[:,0:2])
-#     print('a : ', i, "Accuracy: %0.5f (+/- %0.5f)" % (scores.mean(), scores.std() * 2))
-#
-#     y_pred = pls.predict(X_train)
-#     # The coefficients
-#     #print('Coefficients: \n', pls.coef_)
-#     ## The mean squared error
-#     print("Mean squared error: %.3f" % mean_squared_error(y_train[:,0:2], y_pred))
-#     # Explained variance score: 1 is perfect prediction
-#     print('Variance score: %.3f' % r2_score(y_train[:,0:2], y_pred, multioutput='variance_weighted'))
-#
-# print('====================================================')
-
-pls = PLSRegression(n_components = 6, scale=False)
-pls.fit(X_train, y_train[:,0:2])
-y_pred = pls.predict(X_train)
-
-y_temp = np.c_[y_train, y_pred]
-y_sort = y_temp[y_temp[:,2].argsort()]
-
-#y_pred = pls.transform(X_train, y_train)
-#a
-# y1_test = y_train_r[:,:1].reshape(len(y_train_r)) + Mean_M[10:11]
-# y1_pred = y_pred_r[:,:1].reshape(len(y_pred_r)) + Mean_M[10:11]
-
-y1_test = y_sort[:,:1].reshape(len(y_train)) + Mean_M[10:11]
-y1_pred = y_sort[:,3:4].reshape(len(y_pred)) + Mean_M[10:11]
-
-N_Queue = Sampling_M[:,10:12]
-
-# plt.plot(np.arange(N), y1_test, 'bx--', y1_pred,'rx--', linewidth=2)
-# plt.xlabel('Run No.')
-# plt.ylabel('Y_value')
-
-def sampling_DOE(k):
     u1_p1 = np.random.normal(0.4, np.sqrt(0.2))
     u2_p1 = np.random.normal(0.6, np.sqrt(0.2))
     u_p1 = np.array([u1_p1, u2_p1])
@@ -142,6 +48,45 @@ def sampling_DOE(k):
     rows = np.r_[psi, y_p1]
     return rows
 
+def plt_show(y1_act, y1_pred):
+    plt.plot(np.arange(N), y1_act, 'bx--', y1_pred,'rx--', linewidth=2)
+    plt.xlabel('Run No.')
+    plt.ylabel('y_value')
+
+for k in range(0, N): # range(101) = [0, 1, 2, ..., 100])
+    result = sampling(k)
+    DoE_Queue.append(result)
+
+npDoE_Queue = np.array(DoE_Queue)
+DoE_Mean = np.mean(npDoE_Queue, axis = 0)
+
+plsModelData = np.c_[npDoE_Queue - DoE_Mean, np.arange(N)]
+
+V0 = plsModelData[:,0:10]
+Y0 = plsModelData[:,10:13]
+
+V_train, V_test, Y_train, Y_test = train_test_split(V0, Y0, test_size=0)
+
+pls = PLSRegression(n_components = 6, scale=False)
+pls.fit(V_train, Y_train[:,0:2])
+Y_pred = pls.predict(V_train)
+
+Y_temp = np.c_[Y_train, Y_pred]
+Y_sort = Y_temp[Y_temp[:,2].argsort()]
+
+y1_act = Y_sort[:,:1].reshape(len(Y_train)) + DoE_Mean[10:11]
+y1_pred = Y_sort[:,3:4].reshape(len(Y_pred)) + DoE_Mean[10:11]
+
+print("Mean squared error: %.3f" % mean_squared_error(y1_act, y1_pred))
+# plt_show(y1_act, y1_pred)
+
+actV0 = npDoE_Queue[:,0:10]
+actY0 = npDoE_Queue[:,10:12]
+predY0 = np.array(Y_sort[:,3:5] + DoE_Mean[10:12])
+meanV0 = DoE_Mean[0:10]
+meanY0 = DoE_Mean[10:12]
+
+
 def pls_Update(queue, ez, mean_Z):
     np_queue = np.array(queue)
     np_queue[1:10, 0:10] = lamda_PLS * np_queue[1:10, 0:10]
@@ -153,16 +98,10 @@ def pls_Update(queue, ez, mean_Z):
 
 
 W_Queue = []
-mean_Z = Mean_M  ## V0, Y0 Mean Center
-#W_Queue.append(np.array(mean_Z))
-
-#Y_Queue = []
-
-Z = 40
-#Z = 3
+meanV = meanV0
+meanY = meanY0  ## V0, Y0 Mean Center
+Z = 1
 M = 10
-# t = np.array(W_Queue)
-# t[1:M,1]
 ez = []
 ez.append([0,0])
 result_k = []
@@ -193,29 +132,29 @@ for z in np.arange(0, Z):
 #       del W_Queue[1:M + 1]
 
     for k in np.arange(z * M + 1, ((z + 1) * M) + 1):
-        result_k = sampling_DOE(k)
-        Vz = result_k[0:10] - mean_Z.ravel()[0:10]
+        resultV = sampling(k)
+        Vz = resultV[0:10] - meanV
 
-        y_pred = pls.predict(Vz.reshape(1, 10)) + mean_Z[12:14]
+        y_pred = pls.predict(Vz.reshape(1, 10)) + meanY
 
-        rows = np.r_[result_k, y_pred.reshape(2,)]
-        #print(rows)
-        y1_pred1.append(rows[12:14])
-        y1_act1.append(rows[10:12])
-        W_Queue.append(rows)
+        # rows = np.r_[result_k, y_pred.reshape(2,)]
+        # #print(rows)
+        # y1_pred1.append(rows[12:14])
+        # y1_act1.append(rows[10:12])
+        # W_Queue.append(rows)
 
-        #print(len(W_Queue))
+        print(y_pred)
 
 #y_value = np.array(ez[0])
 
-y1_act = np.array(y1_act1)
-y1_pred = np.array(y1_pred1)
-
-print("Mean squared error: %.3f" % mean_squared_error(y1_act[:,0:1], y1_pred[:,0:1]))
-
-plt.plot(np.arange(N), y1_act[:,0:1], 'bx--', y1_pred[:,0:1],'rx--', linewidth=2)
-plt.xlabel('Run No.')
-plt.ylabel('Y_value')
+# y1_act = np.array(y1_act1)
+# y1_pred = np.array(y1_pred1)
+#
+# #print("Mean squared error: %.3f" % mean_squared_error(y1_act[:,0:1], y1_pred[:,0:1]))
+#
+# plt.plot(np.arange(N), y1_act[:,0:1], 'bx--', y1_pred[:,0:1],'rx--', linewidth=2)
+# plt.xlabel('Run No.')
+# plt.ylabel('Y_value')
 #
 # ot = np.array(ez)
 #
