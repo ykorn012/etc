353ba668218d38a09a4b5d0b8206973a6990379d
크게접은하늘 <ykorn012@gmail.com>
2018-07-20 01:55:20 +0900

    Update FwcRun.py

--- FwcRun.py
+++ FwcRun.py
@@ -2,20 +2,29 @@ from simulator.FwcSimulator import FwcSimulator
 import os
 import numpy as np
 
-Tgt = np.array([0, 50])
+Tgt_p1 = np.array([0, 50])
 A_p1 = np.array([[0.5, -0.2], [0.25, 0.15]])
 d_p1 = np.array([[0.1, 0], [0.05, 0]])
 C_p1 = np.transpose(np.array([[0, 0.5, 0.05, 0, 0.15, 0], [0.085, 0, 0.025, 0.2, 0, 0]]))
 
+Tgt_p2 = np.array([0, 0])
+A_p2 = np.array([[1, 0.1], [-0.5, 0.2]])
+d_p2 = np.array([[0, 0.05], [0, 0.05]])
+C_p2 = np.transpose(np.array([[0.1, 0, 0, -0.2, 0.1], [0, -0.2, 0, 0.3, 0]]))
+F_p2 = np.array([[0.05, 0], [0, 0.05]])
+
 def main():
     os.chdir("D:/01. CLASS/Machine Learning/")
-    fwc_r2r = FwcSimulator(Tgt, A_p1, d_p1, C_p1)
+    fwc_r2r = FwcSimulator(Tgt_p1, A_p1, d_p1, C_p1, None)
     fwc_r2r.DoE_Run(lamda_PLS=1, dEWMA_Wgt1=0.45, dEWMA_Wgt2=0.35, Z=10, M=10, isR2R=True)
     fwc_r2r.VM2R2R_Run(lamda_PLS=1, dEWMA_Wgt1=0.45, dEWMA_Wgt2=0.35, Z=20, M=10, isR2R=True)
 
-    fwc_l2l = FwcSimulator(Tgt, A_p1, d_p1, C_p1)
+    fwc_l2l = FwcSimulator(Tgt_p1, A_p1, d_p1, C_p1, None)
     fwc_l2l.DoE_Run(lamda_PLS=1, dEWMA_Wgt1=0.45, dEWMA_Wgt2=0.35, Z=10, M=10, isR2R=False)
-    fwc_l2l.VM2R2R_Run(lamda_PLS=1, dEWMA_Wgt1=0.45, dEWMA_Wgt2=0.35, Z=20, M=10, isR2R=False)
+    p1_pls_weight = fwc_l2l.VM2R2R_Run(lamda_PLS=1, dEWMA_Wgt1=0.45, dEWMA_Wgt2=0.35, Z=20, M=10, isR2R=False)
+
+    print(p1_pls_weight)
+    fwc_pre_r2r = FwcSimulator(Tgt_p2, A_p2, d_p2, C_p2, F_p2)
 
 
 
