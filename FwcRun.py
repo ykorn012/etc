4012a62e401cc27f59631faadf18eb6d34e2bd2b
크게접은하늘 <ykorn012@gmail.com>
2018-07-30 02:28:19 +0900

    Update FwcRun.py

--- FwcRun.py
+++ FwcRun.py
@@ -1,5 +1,5 @@
-from simulator.FwcSimulator import FwcSimulator
-import os
+from simulator.FWC_P1_Simulator import FWC_P1_Simulator
+from simulator.FWC_P2_Simulator import FWC_P2_Simulator
 import numpy as np
 
 Tgt_p1 = np.array([0, 50])
@@ -14,22 +14,31 @@ C_p2 = np.transpose(np.array([[0.1, 0, 0, -0.2, 0.1], [0, -0.2, 0, 0.3, 0]]))
 F_p2 = np.array([[0.05, 0], [0, 0.05]])
 
 def main():
-    os.chdir("D:/01. CLASS/Machine Learning/")
-    fwc_p1_r2r = FwcSimulator(Tgt_p1, A_p1, d_p1, C_p1, None)
-    fwc_p1_r2r.DoE_Run(lamda_PLS=1, dEWMA_Wgt1=0.45, dEWMA_Wgt2=0.35, Z=10, M=10, f=None, isR2R=True)
-    # p1_r2r_y_act, p1_r2r_VMOutput = fwc_p1_r2r.VM_Run(lamda_PLS=1, dEWMA_Wgt1=0.45, dEWMA_Wgt2=0.35, Z=20, M=10, f=None, isR2R=True)
-    #
-    # fwc_p1_l2l = FwcSimulator(Tgt_p1, A_p1, d_p1, C_p1, None)
-    # fwc_p1_l2l.DoE_Run(lamda_PLS=1, dEWMA_Wgt1=0.45, dEWMA_Wgt2=0.35, Z=10, M=10, f=None, isR2R=False)
-    # p1_l2l_y_act, p1_l2l_VMOutput = fwc_p1_l2l.VM_Run(lamda_PLS=1, dEWMA_Wgt1=0.45, dEWMA_Wgt2=0.35, Z=20, M=10, f=None, isR2R=False)
-    #
-    # fwc_p1_r2r.plt_show2(200, p1_r2r_y_act[:, 0:1])
-    # fwc_p1_l2l.plt_show1(200, p1_l2l_y_act[:, 0:1])
+    fwc_p1_r2r = FWC_P1_Simulator(Tgt_p1, A_p1, d_p1, C_p1, 1000000)
+    fwc_p1_r2r.DoE_Run(lamda_PLS=1, dEWMA_Wgt1=0.45, dEWMA_Wgt2=0.35, Z=10, M=10, isR2R=True)
+    p1_r2r_VMOutput = fwc_p1_r2r.VM_Run(lamda_PLS=1, dEWMA_Wgt1=0.45, dEWMA_Wgt2=0.35, Z=20, M=10, isR2R=True)
 
-    # fwc_p2_r2r = FwcSimulator(Tgt_p2, A_p2, d_p2, C_p2, None)
-    # fwc_p2_r2r.DoE_Run(lamda_PLS=1, dEWMA_Wgt1=0.45, dEWMA_Wgt2=0.35, Z=10, M=10, f=None, isR2R=True)
-    # p2_r2r_y_act, p2_r2r_VMOutput = fwc_p1_r2r.VM_Run(lamda_PLS=1, dEWMA_Wgt1=0.45, dEWMA_Wgt2=0.35, Z=20, M=10, f=None, isR2R=True)
-    #
+    fwc_p1_l2l = FWC_P1_Simulator(Tgt_p1, A_p1, d_p1, C_p1, 100000)
+    fwc_p1_l2l.DoE_Run(lamda_PLS=1, dEWMA_Wgt1=0.45, dEWMA_Wgt2=0.35, Z=10, M=10, isR2R=False)
+    fwc_p1_l2l.VM_Run(lamda_PLS=1, dEWMA_Wgt1=0.45, dEWMA_Wgt2=0.35, Z=20, M=10, isR2R=False)
+
+    fwc_p2_r2r = FWC_P2_Simulator(Tgt_p2, A_p2, d_p2, C_p2, None, 1000000)
+    fwc_p2_r2r.DoE_Run(lamda_PLS=1, dEWMA_Wgt1=0.45, dEWMA_Wgt2=0.35, Z=10, M=10, f=None, isR2R=True)
+    fwc_p2_r2r.VM_Run(lamda_PLS=1, dEWMA_Wgt1=0.45, dEWMA_Wgt2=0.35, Z=20, M=10, f=None, isR2R=True)
+
+    fwc_p2_pre_r2r = FWC_P2_Simulator(Tgt_p2, A_p2, d_p2, C_p2, F_p2, 100000)
+    fwc_p2_pre_r2r.DoE_Run(lamda_PLS=1, dEWMA_Wgt1=0.45, dEWMA_Wgt2=0.35, Z=10, M=10, f=p1_r2r_VMOutput, isR2R=True)
+    fwc_p2_pre_r2r.VM_Run(lamda_PLS=1, dEWMA_Wgt1=0.45, dEWMA_Wgt2=0.35, Z=20, M=10, f=p1_r2r_VMOutput, isR2R=True)
+
+    fwc_p2_l2l = FWC_P2_Simulator(Tgt_p2, A_p2, d_p2, C_p2, None, 5000000)
+    fwc_p2_l2l.DoE_Run(lamda_PLS=1, dEWMA_Wgt1=0.45, dEWMA_Wgt2=0.35, Z=10, M=10, f=None, isR2R=False)
+    fwc_p2_l2l.VM_Run(lamda_PLS=1, dEWMA_Wgt1=0.45, dEWMA_Wgt2=0.35, Z=20, M=10, f=None, isR2R=False)
+
+    fwc_p2_pre_r2r = FWC_P2_Simulator(Tgt_p2, A_p2, d_p2, C_p2, F_p2, 100000)
+    fwc_p2_pre_r2r.DoE_Run(lamda_PLS=1, dEWMA_Wgt1=0.45, dEWMA_Wgt2=0.35, Z=10, M=10, f=p1_r2r_VMOutput, isR2R=True)
+    fwc_p2_pre_r2r.VM_Run(lamda_PLS=1, dEWMA_Wgt1=0.45, dEWMA_Wgt2=0.35, Z=20, M=10, f=p1_r2r_VMOutput, isR2R=True)
+
+
 
 if __name__ == "__main__":
     main()
